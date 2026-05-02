671d3f0024f93bbff3dd98ccad82e8543a1b7982
Johnson Towoju <johnsontodimu@gmail.com>
2026-05-02 11:05:50 +0100

    rm shadcn and add nuxt ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
+    modules: ['@nuxt/ui'],
     devtools: { enabled: true },
     css: ['./app/assets/css/globals.css', './app/assets/css/main.sass'],
     vite: {
