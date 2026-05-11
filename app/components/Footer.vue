d8754479ac5298290e9d7c7d9bf6db756463b4dc
Johnson Towoju <johnsontodimu@gmail.com>
2026-05-11 21:00:01 +0100

    feat: update footer styles

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
-    <footer class="p-4 border-t border-t-stone-200 text-center">
-        {{ new Date().getFullYear() }} - Receipt Scanner
+    <footer class="p-4 border-t border-t-stone-200 text-center text-sm text-stone-500">
+        {{ new Date().getFullYear() }} - Receipt Scanner &bull; &copy;
+        <span class="font-semibold tracking-tight"
+            >Craft<span class="text-stone-800">Motion</span></span
+        >
     </footer>
 </template>
 
